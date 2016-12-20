cce1da95c8a9e4db6ee6f584e5ab2bb56faeccad
Stephen Lane-Walsh <slanewalsh@mywebgrocer.com>
2016-12-20 10:08:37 -0500

    Added vec2i and vec2u

--- src/glmm/vec2.h
+++ src/glmm/vec2.h
@@ -32,24 +32,24 @@ typedef glmm_vec2u vec2u;
 
 #endif // GLMM_NO_SHORT_DEFINES
 
-void glmm_vec2_init(glmm_vec2* this);
-void glmm_vec2i_init(glmm_vec2i* this);
-void glmm_vec2u_init(glmm_vec2u* this);
+void glmm_vec2_init(glmm_vec2 *this);
+void glmm_vec2i_init(glmm_vec2i *this);
+void glmm_vec2u_init(glmm_vec2u *this);
 
-bool glmm_vec2_eq(glmm_vec2* left, glmm_vec2* right);
-bool glmm_vec2i_eq(glmm_vec2i* left, glmm_vec2i* right);
-bool glmm_vec2u_eq(glmm_vec2u* left, glmm_vec2u* right);
+bool glmm_vec2_eq(glmm_vec2 *left, glmm_vec2 *right);
+bool glmm_vec2i_eq(glmm_vec2i *left, glmm_vec2i *right);
+bool glmm_vec2u_eq(glmm_vec2u *left, glmm_vec2u *right);
 
-void glmm_vec2_add(glmm_vec2* this, glmm_vec2* other);
-void glmm_vec2i_add(glmm_vec2i* this, glmm_vec2i* other);
-void glmm_vec2u_add(glmm_vec2u* this, glmm_vec2u* other);
+void glmm_vec2_add(glmm_vec2 *this, glmm_vec2 *other);
+void glmm_vec2i_add(glmm_vec2i *this, glmm_vec2i *other);
+void glmm_vec2u_add(glmm_vec2u *this, glmm_vec2u *other);
 
-void glmm_vec2_sub(glmm_vec2* this, glmm_vec2* other);
-void glmm_vec2i_sub(glmm_vec2i* this, glmm_vec2i* other);
-void glmm_vec2u_sub(glmm_vec2u* this, glmm_vec2u* other);
+void glmm_vec2_sub(glmm_vec2 *this, glmm_vec2 *other);
+void glmm_vec2i_sub(glmm_vec2i *this, glmm_vec2i *other);
+void glmm_vec2u_sub(glmm_vec2u *this, glmm_vec2u *other);
 
-void glmm_vec2_print(glmm_vec2* this);
-void glmm_vec2i_print(glmm_vec2i* this);
-void glmm_vec2u_print(glmm_vec2u* this);
+void glmm_vec2_print(glmm_vec2 *this);
+void glmm_vec2i_print(glmm_vec2i *this);
+void glmm_vec2u_print(glmm_vec2u *this);
 
 #endif // GLMM_VEC2_H
